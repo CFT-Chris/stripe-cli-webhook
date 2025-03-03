edaa5009aa391b8aa3203a5f3ffdd17579ffc265
Chris <CFT-Chris@users.noreply.github.com>
2025-03-03 15:32:57 -0500

    Create preinstall.sh

---
+++ preinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ "$(id -u)" = "0" ]; then
+  curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | tee /usr/share/keyrings/stripe.gpg
+  echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | tee -a /etc/apt/sources.list.d/stripe.list
+  apt update
+  apt install -y stripe
+fi
